afbb6f6211341ce22ee9ac9be69415694a56e01e
1nTy <federico.fini.1992@gmail.com>
2014-04-29 13:36:46 +0200

    New engines

---
+++ amazon_it.js
@@ -0,0 +1,14 @@
+eng.amazon = {
+
+	pageTitle: "Amazon",
+	logo: "amazon.png",
+
+	places: {
+		'Search' : ["http://www.amazon.it/s/ref=nb_sb_noss_1?__mk_it_IT=%C3%85M%C3%85%C5%BD%C3%95%C3%91&url=search-alias%3Daps&field-keywords=%query%", false]
+	},
+
+	languages: {
+		'IT': 'it',
+	}
+
+};
